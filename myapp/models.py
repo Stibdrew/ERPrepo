2d2ab6dd4b90365fef3b4c3c03f2d88fd4b9f43b
STEVEN RAQUEDAN <scraquedan1@student.hau.edu.ph>
2024-10-16 09:40:15 +0800

    tang

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,6 @@
-# myapp/models.py
-
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,9 +8,17 @@ class UserProfile(models.Model):
     full_name = models.CharField(max_length=255)
     mobile_number = models.CharField(max_length=15)
     business_title = models.CharField(max_length=255)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # Add this line
 
     def __str__(self):
-        return self.full_name  #
+        return self.full_name
+
+
+
+class UserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['company_name', 'full_name', 'mobile_number', 'business_title', 'balance']  # Include balance
 
 
 
